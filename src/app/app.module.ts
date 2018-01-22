958fa0029c2fa600ec582f43b93be93545b4bf57
Tibytt <francisco.j.tibytt@accenture.com>
2018-01-22 09:37:11 -0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-const appRoutes: Routes= [ {path:'',component: ProductListComponent},{path:'products',component: ProductListComponent}, {path:'product/:id',component: ProductPageComponent}];
+const appRoutes: Routes= [ {path:'products',component: ProductListComponent}, {path:'product/:id',component: ProductPageComponent}, {path:'', redirectTo: 'products', pathMatch:'full'}];
 
 
 
